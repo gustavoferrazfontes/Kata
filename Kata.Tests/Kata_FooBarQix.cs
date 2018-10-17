4e2be63901fdc7ab66117c20076e9402f262f970
Gustavo Fontes <gustavo.ferrazfontes@gmail.com>
2018-10-17 12:54:34 -0300

    implementation about Kata FooBarQix

---
+++ Kata.Tests/Kata_FooBarQix.cs
@@ -0,0 +1,84 @@
+﻿using FluentAssertions;
+using Kata.model.Katas;
+using Xunit;
+
+namespace Kata.Tests
+{
+    public class Kata_KarateChop
+    {
+        [Fact]
+        public void ShouldPrintFooFoo()
+        {
+            FooBarQix.Print(3).Should().Be("FooFoo");
+        }
+
+        [Fact]
+        public void ShouldPrintNumberWhenNotDivisibleByThree()
+        {
+            FooBarQix.Print(1).Should().Be("1");
+        }
+
+        [Fact]
+        public void ShouldPrintBar()
+        {
+            FooBarQix.Print(10).Should().Be("Bar*");
+        }
+
+        [Fact]
+        public void ShouldPrintQix()
+        {
+            FooBarQix.Print(7).Should().Be("QixQix");
+        }
+
+        [Fact]
+        public void ShouldPrintFooBar()
+        {
+            FooBarQix.Print(51).Should().Be("FooBar");
+        }
+        [Fact]
+        public void ShouldPrintBarFoo()
+        {
+            FooBarQix.Print(53).Should().Be("BarFoo");
+        }
+
+        [Fact]
+        public void ShouldPrintFooFooFoo()
+        {
+            FooBarQix.Print(33).Should().Be("FooFooFoo");
+        }
+
+        [Fact]
+        public void ShouldPrintFooBarBar()
+        {
+            FooBarQix.Print(15).Should().Be("FooBarBar");
+        }
+
+        [Fact]
+        public void ShouldPrintFooQix()
+        {
+            FooBarQix.Print(21).Should().Be("FooQix");
+        }
+
+        [Fact]
+        public void ShouldPrintEigth()
+        {
+            FooBarQix.Print(8).Should().Be("8");
+        }
+
+        [Fact]
+        public void ShouldPrintFoo()
+        {
+            FooBarQix.Print(9).Should().Be("Foo");
+        }
+
+        [Fact]
+        public void ShouldPrintBarWithAsterisk()
+        {
+            FooBarQix.Print(101).Should().Be("1*1");
+            FooBarQix.Print(303).Should().Be("FooFoo*Foo");
+            FooBarQix.Print(105).Should().Be("FooBarQix*Bar");
+            FooBarQix.Print(10101).Should().Be("FooQix**");
+        }
+
+    }
+}
---
+++ Kata.model/Katas/FooBarQix.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using static Kata.model.Katas.FooBarQixSpec;
+
+namespace Kata.model.Katas
+{
+    public class FooBarQix
+    {
+
+        public string Word { get; private set; }
+        public int Divisible { get; private set; }
+
+        private static List<FooBarQix> FooBarQixTable = new List<FooBarQix>
+        {
+            new FooBarQix{Divisible = 3, Word="Foo"},
+            new FooBarQix{Divisible = 5, Word="Bar"},
+            new FooBarQix{Divisible = 7, Word="Qix"},
+            new FooBarQix{Divisible = 0, Word="*"}
+        };
+        public static string Print(int value)
+        {
+
+            var modResult =
+                    FooBarQixTable
+                        .Where(ModIsZero(value))
+                        .Aggregate(new StringBuilder(), (word, nextLetter) => word.AppendFormat(nextLetter.Word)).ToString();
+
+            var numberContainsResult = value
+                    .ToString()
+                    .SelectMany(WhereFooBarQixTableDivisableContainsValue(FooBarQixTable))
+                    .Aggregate(new StringBuilder(),
+                    (word, nextLetter) => word.AppendFormat(nextLetter.Word)).ToString();
+
+            var wordResult = string.Join("", modResult, numberContainsResult);
+
+            return wordResult.Length < 3 ? value.ToString().Replace('0', '*') : wordResult;
+        }
+
+        private static IOrderedEnumerable<FooBarQix> GetWordFromDivisibleNumberThatContains(char letter)
+            => FooBarQixTable.Where(fooBarQixItem =>
+                fooBarQixItem.Divisible.ToString().Contains(letter)).OrderBy(item => item);
+    }
+
+    public static class FooBarQixSpec
+    {
+        public static Func<FooBarQix, bool> ModIsZero(int value)
+                => foobarqix => foobarqix.Divisible != 0 && value % foobarqix.Divisible == 0;
+        public static Func<char, IEnumerable<FooBarQix>> WhereFooBarQixTableDivisableContainsValue(IEnumerable<FooBarQix> fooBarQixTable)
+                        => letter => fooBarQixTable.Where(fooBarQixItem =>
+                fooBarQixItem.Divisible.ToString().Contains(letter)).OrderBy(item => item);
+    }
+}
