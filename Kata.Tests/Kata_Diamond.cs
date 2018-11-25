30ae05754634e34588e2e7ecea0ba9f46014b0cc
Gustavo Fontes <gustavo.ferrazfontes@gmail.com>
2018-11-25 20:14:16 -0200

    Kata diamond done - http://codingdojo.org/kata/Diamond/

---
+++ Kata.Tests/Kata_Diamond.cs
@@ -0,0 +1,37 @@
+﻿using FluentAssertions;
+using Kata.model.Katas;
+using Xunit;
+
+namespace Kata.Tests
+{
+    public class Kata_Diamond
+    {
+
+        [Fact]
+        public void ShouldPrintLetterSequence()
+        {
+            var result = Diamond.PrintUntil("F");
+            result.Should().BeEquivalentTo(new string[] { "A", "B", "C", "D", "E", "F" });
+
+        }
+
+
+        [Fact]
+        public void SouldPrintLetterSequenceIdented()
+        {
+            var result = Diamond.Print("C");
+            result.Should()
+                .Be("\n   A\n  B B\n C   C\n  B B\n   A");
+        }
+
+        [Fact]
+        public void SouldPrintLetterBigSequenceIdented()
+        {
+            var result = Diamond.Print("J");
+            result.Should()
+                .Be("\n          A\n         B B\n        C   C\n       D     D\n      E       E\n     F         F\n    G           G\n   H             H\n  I               I\n J                 J\n  I               I\n   H             H\n    G           G\n     F         F\n      E       E\n       D     D\n        C   C\n         B B\n          A");
+        }
+
+
+    }
+}
---
+++ Kata.model/Katas/Diamond.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Kata.model.Katas
+{
+    public class Diamond
+    {
+
+        private static readonly List<string> Alphabet = new List<string>
+        {
+            "A", "B", "C", "D","E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "X", "Y", "Z"
+        };
+
+        public static List<string> PrintUntil(string letter)
+            => GetAlphabetSequenceUntil(GetPositionOf(letter));
+
+        public static string Print(string letter)
+        {
+            var lettersSelectedToBuildDiamond = PrintUntil(letter);
+            var firstPart = BuildUpside(lettersSelectedToBuildDiamond);
+
+            var diamond = BottomSide(lettersSelectedToBuildDiamond, firstPart);
+            return diamond.ToString();
+        }
+
+        private static StringBuilder BottomSide(List<string> lettersSelectedToBuildDiamond, StringBuilder upside)
+        {
+            var sb = new StringBuilder(upside.ToString());
+            for (int i = 0; i < lettersSelectedToBuildDiamond.Count; i++)
+            {
+                sb.AppendFormat(
+                    "{0}{1}{2}",
+                    i == 0 ? string.Empty : "\n",
+                    i == 0 || i == lettersSelectedToBuildDiamond.Count
+                        ? string.Empty
+                        : lettersSelectedToBuildDiamond[lettersSelectedToBuildDiamond.Count - (i + 1)].PadLeft(i + 2),
+                    i == 0 || i == lettersSelectedToBuildDiamond.Count - 1
+                    ? string.Empty :
+                    lettersSelectedToBuildDiamond[lettersSelectedToBuildDiamond.Count - (i + 1)].PadLeft((lettersSelectedToBuildDiamond.Count - (i + 1)) * 2));
+            }
+
+            return sb;
+        }
+
+        private static StringBuilder BuildUpside(List<string> lettersSelectedToBuildDiamond)
+        {
+            var sb = new StringBuilder();
+            for (int i = 0; i < lettersSelectedToBuildDiamond.Count; i++)
+            {
+                sb.AppendFormat("\n{0}{1}",
+                         lettersSelectedToBuildDiamond[i].PadLeft((lettersSelectedToBuildDiamond.Count + 1) - i),
+                    i == 0 || i == lettersSelectedToBuildDiamond.Count
+                        ? string.Empty
+                        : lettersSelectedToBuildDiamond[i].PadLeft((i * 2)));
+            }
+            return sb;
+        }
+
+        private static List<string> GetAlphabetSequenceUntil(int endpoint) => Alphabet.Take(endpoint).ToList();
+        private static int GetPositionOf(string letter) => Alphabet.FindIndex(item => item == letter.ToUpper()) + 1;
+    }
+}
